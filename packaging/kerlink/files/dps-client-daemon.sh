cb0fdd1b83f5805322d2100c431f356e6229eae4
Benoit Duffez <b.duffez@kerlink.com>
2024-03-27 12:50:06 -0700

    file name is EUI, id is eui

--- packaging/kerlink/files/dps-client-daemon.sh
+++ packaging/kerlink/files/dps-client-daemon.sh
@@ -9,7 +9,7 @@ TEMP_CONFIG_FILE="$CONFIG_FILE.tmp"
 OLD_CONFIG_FILE="/etc/chirpstack-gateway-bridge/chirpstack-gateway-bridge.toml"
 
 obtain_gwid() {
-    DEVEUI=$(python3 -c 'from keroslib import utils; print(utils.getEUI64())' | tr '[:upper:]' '[:lower:]')
+    DEVEUI=$(python3 -c 'from keroslib import utils; print(utils.getEUI64())')
 
     return 1
 }
@@ -19,7 +19,8 @@ obtain_gwid
 while :
 do
     mkdir -p $CONFIG_DIR
-    DPS_CLIENT_ARGS="-i $DEFAULT_CONFIG_FILE -r $DEVEUI -c $CERT_DIR/$DEVEUI.cert.pem -k $CERT_DIR/$DEVEUI.key.pem -o $TEMP_CONFIG_FILE"
+    GW_ID=$(echo "$DEVEUI" | tr '[:upper:]' '[:lower:]')
+    DPS_CLIENT_ARGS="-i $DEFAULT_CONFIG_FILE -r $GW_ID -c $CERT_DIR/$DEVEUI.cert.pem -k $CERT_DIR/$DEVEUI.key.pem -o $TEMP_CONFIG_FILE"
     /user/mydevices/dps-client $DPS_CLIENT_ARGS
     RESTART_CHIRPSTACK=false
     if test -f "$OLD_CONFIG_FILE"; then
